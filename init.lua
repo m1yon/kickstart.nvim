afa7ed3f71d05a4ccb2173fda2c8672c43423c1e
Michael Lyon <mlyon360@gmail.com>
2025-06-23 11:31:49 -0600

    add go docs plugin

--- init.lua
+++ init.lua
@@ -260,6 +260,7 @@ require('lazy').setup({
   require 'csv',
   require 'plugins/snacks',
   require 'plugins/mini',
+  require 'docs',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
