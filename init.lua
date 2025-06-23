447600f02057ff275998e31fc3d2a25e1cd655f2
Michael Lyon <mlyon360@gmail.com>
2025-06-22 20:43:39 -0600

    remove kickstart folder

--- init.lua
+++ init.lua
@@ -256,14 +256,10 @@ require('lazy').setup({
   require 'help',
   require 'comments',
   require 'editing',
+  require 'linting',
   require 'csv',
   require 'plugins/snacks',
   require 'plugins/mini',
-
-  require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
