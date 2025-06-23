6f755933992e1a429100d34ac7f86f65eb96a1c4
Michael Lyon <mlyon360@gmail.com>
2025-06-23 11:16:38 -0600

    adjust dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,7 +6,16 @@ return {
     ---@type snacks.Config
     opts = {
       bigfile = { enabled = true },
-      dashboard = { enabled = true },
+      dashboard = {
+        enabled = true,
+        sections = {
+          { section = 'header' },
+          { section = 'keys', gap = 1, padding = 1 },
+          { pane = 2, icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1, height = 3 },
+          { pane = 2, icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1, height = 3 },
+          { section = 'startup' },
+        },
+      },
       indent = { enabled = true, animate = { enabled = false } },
       input = { enabled = true },
       picker = { enabled = true },
