cbc2c97410e9ecb4aa575853ce442450a649123e
Michael Lyon <mlyon360@gmail.com>
2025-07-03 11:10:32 -0600

    change supermaven keybind

--- lua/ai.lua
+++ lua/ai.lua
@@ -2,7 +2,9 @@ return {
   {
     'supermaven-inc/supermaven-nvim',
     config = function()
-      require('supermaven-nvim').setup {}
+      require('supermaven-nvim').setup { keymaps = {
+        accept_suggestion = '<s-Tab>',
+      } }
     end,
   },
 }
