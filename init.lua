3e03d092a11f7595d6cdaa8cb7fe9b281a0abcf4
Michael Lyon <mlyon360@gmail.com>
2025-07-14 15:44:23 -0600

    add go debugger

--- init.lua
+++ init.lua
@@ -262,6 +262,7 @@ require('lazy').setup({
   require 'plugins/mini',
   require 'docs',
   require 'ai',
+  require 'debugging',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
