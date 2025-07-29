84040c762b2ae875b0eb19c628ddf7daefd6c425
Michael Lyon <mlyon360@gmail.com>
2025-07-29 10:55:06 -0600

    remove vale

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -164,7 +164,6 @@ return {
         ruff = {},
         biome = {},
         jsonls = {},
-        vale_ls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -207,7 +206,6 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-        'vale-ls',
         'gofumpt',
         'goimports',
         'golines',
