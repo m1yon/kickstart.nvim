f40b978ca94181d2099782b652f1613d0d43519a
Michael Lyon <mlyon360@gmail.com>
2025-09-09 17:22:23 -0600

    revert theme change

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,11 +1,19 @@
 return {
-  {
-    'nyoom-engineering/oxocarbon.nvim',
-    priority = 1000,
-    lazy = false,
+  { -- You can easily change to a different colorscheme.
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is.
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'catppuccin/nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    name = 'catppuccin',
     config = function()
-      vim.opt.background = 'dark' -- set this to dark or light
-      vim.cmd.colorscheme 'oxocarbon'
+      ---@diagnostic disable-next-line: missing-fields
+      require('catppuccin').load()
+
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      vim.cmd.colorscheme 'catppuccin-macchiato'
     end,
   },
 }
