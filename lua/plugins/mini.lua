e202487720cb272462c258ed94f855bc4754c10d
Michael Lyon <mlyon360@gmail.com>
2025-09-09 15:37:15 -0600

    replace which-key with mini.clue

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -53,6 +53,58 @@ return {
       require('mini.pick').setup()
       require('mini.extra').setup()
 
+      local clue = require 'mini.clue'
+      require('mini.clue').setup {
+        triggers = {
+          -- Leader triggers
+          { mode = 'n', keys = '<Leader>' },
+          { mode = 'x', keys = '<Leader>' },
+
+          -- Built-in completion
+          { mode = 'i', keys = '<C-x>' },
+
+          -- `g` key
+          { mode = 'n', keys = 'g' },
+          { mode = 'x', keys = 'g' },
+
+          -- Marks
+          { mode = 'n', keys = "'" },
+          { mode = 'n', keys = '`' },
+          { mode = 'x', keys = "'" },
+          { mode = 'x', keys = '`' },
+
+          -- Registers
+          { mode = 'n', keys = '"' },
+          { mode = 'x', keys = '"' },
+          { mode = 'i', keys = '<C-r>' },
+          { mode = 'c', keys = '<C-r>' },
+
+          -- Window commands
+          { mode = 'n', keys = '<C-w>' },
+
+          -- `z` key
+          { mode = 'n', keys = 'z' },
+          { mode = 'x', keys = 'z' },
+        },
+
+        window = {
+          delay = 0,
+        },
+
+        clues = {
+          -- Enhance this by adding descriptions for <Leader> mapping groups
+          clue.gen_clues.builtin_completion(),
+          clue.gen_clues.g(),
+          clue.gen_clues.marks(),
+          clue.gen_clues.registers(),
+          clue.gen_clues.windows(),
+          clue.gen_clues.z(),
+          { mode = 'n', keys = '<Leader>f', desc = '+Find' },
+          { mode = 'n', keys = '<Leader>l', desc = '+LSP' },
+          { mode = 'n', keys = '<Leader>g', desc = '+Git' },
+        },
+      }
+
       local starter = require 'mini.starter'
       starter.setup {
         items = {
